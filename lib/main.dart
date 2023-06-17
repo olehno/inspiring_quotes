18fa369e5d02bc38f7db8596c4cf220bcf70cb76
Arthur <olehno.arturs@gmail.com>
2023-06-18 00:09:03 +0300

    - FAAO-17 - add images - add images to pubspec.yaml - change main file - add list of quotes - add structure of quotes - make UI - add random number generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:inspiring_quotes/quote_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: QuotePage(),
+    ),
+  );
 }
-
---
+++ lib/quotes.dart
@@ -0,0 +1,10 @@
+import 'package:inspiring_quotes/quote_info.dart';
+
+final quotes = [
+  const QuoteInfo(imageName: "1"),
+  const QuoteInfo(imageName: "2"),
+  const QuoteInfo(imageName: "3"),
+  const QuoteInfo(imageName: "4"),
+  const QuoteInfo(imageName: "5"),
+  const QuoteInfo(imageName: "6"),
+];
